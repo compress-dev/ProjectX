1cbe632415732936a3194bf30818d9811c2527fc
mr-exception <drabandi1996@gmail.com>
2017-12-11 07:56:35 +0330

    debug of crossover and mutations finished

--- mutation.js
+++ mutation.js
@@ -1,7 +1,7 @@
 var mutation_a = (chromosome, mute_probability, m, n) => {
   for(var i=0; i<chromosome.length; i++){
     if(Math.random() < mute_probability){
-      console.log(`mute on gene ${i}`)
+      // console.log(`mute on gene ${i}`)
       chromosome[i] = Math.floor(Math.random() *m)
     }
   }
@@ -13,7 +13,7 @@ var mutation_b = (chromosome, mute_probability, m, n) => {
   for(var i=0; i<swap_counts; i++){
     var first_index = Math.floor(Math.random() *n)
     var second_index = Math.floor(Math.random() *n)
-    console.log(`swap on ${first_index} and ${second_index}`)
+    // console.log(`swap on ${first_index} and ${second_index}`)
     var temp = chromosome[first_index]
     chromosome[first_index] = chromosome[second_index]
     chromosome[second_index] = temp
