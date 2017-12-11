d78c9467e60f5eb85f73b5e251573bf8ad6d316d
mr-exception <drabandi1996@gmail.com>
2017-12-11 07:40:12 +0330

    mutation added

---
+++ mutation.js
@@ -0,0 +1,27 @@
+var mutation_a = (chromosome, mute_probability, m, n) => {
+  for(var i=0; i<chromosome.length; i++){
+    if(Math.random() < mute_probability){
+      console.log(`mute on gene ${i}`)
+      chromosome[i] = Math.floor(Math.random() *m)
+    }
+  }
+  return chromosome
+}
+
+var mutation_b = (chromosome, mute_probability, m, n) => {
+  var swap_counts = Math.floor(mute_probability *n)
+  for(var i=0; i<swap_counts; i++){
+    var first_index = Math.floor(Math.random() *n)
+    var second_index = Math.floor(Math.random() *n)
+    console.log(`swap on ${first_index} and ${second_index}`)
+    var temp = chromosome[first_index]
+    chromosome[first_index] = chromosome[second_index]
+    chromosome[second_index] = temp
+  }
+  return chromosome
+}
+
+module.exports = {
+  mutation_a,
+  mutation_b,
+}
